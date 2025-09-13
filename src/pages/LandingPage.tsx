329b0f70c5e1fdec8cc5c912c596371f348ae801
Michael Li <michael.mingdao.li@gmail.com>
2025-09-13 10:24:55 -0400

    Hotfix margin on landing

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -336,7 +336,7 @@ const LandingPage: React.FC = () => {
                       }
                     }}
                   >
-                    <svg width="100%" height="100%" viewBox="0 0 300 340" preserveAspectRatio="xMidYMid meet">
+                    <svg width="100%" height="100%" viewBox="0 0 300 280" preserveAspectRatio="xMidYMid meet">
                       {/* Glow backdrop */}
                       <defs>
                         <filter id="glow" x="-50%" y="-50%" width="200%" height="200%">
